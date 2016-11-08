8c23af7840e3316822a2b6d026612aecaf69e878
chou <zxnzysj@163.com>
2016-11-08 15:44:08 +0800

    update archive about_me taget

--- article/views.py
+++ article/views.py
@@ -1,12 +1,15 @@
+#coding:utf-8
 from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
 from django.http import Http404
 
+
 def home(request):
     post_list = Article.objects.all()
     return render(request, 'home.html', {'post_list': post_list})
+
 
 def detail(request, id):
     try:
@@ -15,3 +18,22 @@ def detail(request, id):
         raise Http404
     return render(request, 'post.html', {'post': post})
 
+
+def archives(request):
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'archives.html', {'post_list': post_list})
+
+
+def about_me(request):
+    return render(request, 'aboutme.html')
+
+
+def search_tag(request, tag):
+    try:
+        post_list = Article.objects.filter(category__iexact=tag)#忽略大小写, __contains:包含
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'tag.html', {'post_list': post_list}) 
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -20,5 +20,8 @@ from article import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
-    url(r'^(?P<id>\d+)/$', views.detail, name='detail')
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
+    url(r'^archives/$', views.archives, name='archives'),
+    url(r'^aboutme/$', views.about_me, name='about_me'),
+    url(r'^tag_(?P<tag>\w+)/$', views.search_tag, name='search_tag'),
 ]
