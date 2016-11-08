22ecabb95ac772b50c08d8fb0db8bfb3f0111261
chou <zxnzysj@163.com>
2016-11-08 13:58:23 +0800

    update duoshuo markdown

--- article/models.py
+++ article/models.py
@@ -1,12 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
     category = models.CharField(max_length=50, blank=True)
     date_time = models.DateTimeField(auto_now_add=True)
     content = models.TextField(blank=True, null=True)
+
+    def get_absolute_url(self):
+        path = reverse('detail', kwargs={'id': self.id})
+        return 'http://192.168.31.198:8000%s' % path
 
     def __unicode__(self):
         return self.title
--- article/views.py
+++ article/views.py
@@ -2,18 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
+from django.http import Http404
 
 def home(request):
-    #return HttpResponse('Hello World, Django')
     post_list = Article.objects.all()
     return render(request, 'home.html', {'post_list': post_list})
 
-def detail(request, my_args):
-    #return HttpResponse("You're looking at my_args %s." % my_args)
-    post = Article.objects.all()[int(my_args)]
-    string = ('title = %s, category = %s, date_time = %s, content = %s' 
-        % (post.title, post.category, post.date_time, post.content)
-    )
-    return HttpResponse(string)
-
+def detail(request, id):
+    try:
+        post = Article.objects.get(pk=id)
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post': post})
 
