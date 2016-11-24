efb49bcf9453cf5cb5ebaa4d604371e97ce1a2f3
Hohenheim <zxnzysj@163.com>
2016-11-24 10:38:36 +0800

    update admin page

--- article/admin.py
+++ article/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from article.models import Article
 
-admin.site.register(Article)
+
+class BlogAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['title', 'category', 'content']}),
+    ]
+    list_display = ('title', 'category', 'date_time')
+    list_filter = ['category', 'date_time']
+    search_fields = ['title']
+
+admin.site.register(Article, BlogAdmin)
