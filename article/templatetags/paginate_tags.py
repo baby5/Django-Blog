1bbda02757a0b0feb7348b965681769a974a8bea
baby5 <zxnzysj@163.com>
2016-12-13 22:14:07 +0800

    update page

---
+++ article/templatetags/paginate_tags.py
@@ -0,0 +1,42 @@
+from django import template
+from django.cor.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def paginate(context, object_list, page_count):
+    paginator = Paginator(object_list, page_count)
+    page = context['request'].GET.get('page')
+
+    try:
+        object_list = paginator.page(page)
+        context['current_page'] = int(page)
+        pages = get_page_range(page, paginator.num_pages)
+    except PageNotAnInteger:
+        object_list = paginator.page(1)
+        context['current_page'] = 1
+        pages = get_page_range(1, paginator.num_pages)
+    except EmptyPage:
+        object_list = paginator.page(paginator.num_pages)
+        context['current_page'] = paginator.num_pages
+        pages = get_page_range(paginator.num_pages, paginator.num_pages)
+
+    context['article_list'] = object_list
+    context['pages'] = pages
+    context['last_page'] = paginator.num_pages
+    context['first_page'] = 1
+    try:
+        context['pages_first'] = pages[0]
+        context['pages_last'] = pages[-1] + 1
+    except:
+        context['pages_first'] = 1
+        context['pages_last'] = 2
+
+    return ''
+
+
+def get_page_range(page, num_pages):
+    left = page-2 if page-2 > 1 else 2
+    right = page+2 if page+2 < num_pages else num_pages
+
+    return range(left, right)
